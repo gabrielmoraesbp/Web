57b79e78599e0dc550a9093bd60e77895cf2d209
Gabriel-dev <gabrielmoraes_r@hotmail.com>
2020-10-26 19:47:49 -0300

    database created

---
+++ Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
@@ -0,0 +1,38 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Aula1
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Produto>().HasKey(t => t.Id);
+
+            modelBuilder.Entity<Pedido>().HasKey(t => t.Id);
+            //1 pedido tem muitos Itemspedidos, cria relacionamento entre as tabelas.
+            modelBuilder.Entity<Pedido>().HasMany(t => t.Itens).WithOne(t => t.Pedido);
+            //cada pedido tem um cadastro
+            modelBuilder.Entity<Pedido>().HasOne(t => t.Cadastro).WithOne(t => t.Pedido).IsRequired();
+
+            modelBuilder.Entity<ItemPedido>().HasKey(t => t.Id);
+            //o item pedido esta relacionado a 1 pedido e 1 produto
+            modelBuilder.Entity<ItemPedido>().HasOne(t => t.Pedido);
+            modelBuilder.Entity<ItemPedido>().HasOne(t => t.Produto);
+
+            modelBuilder.Entity<Cadastro>().HasKey(t => t.Id);
+            modelBuilder.Entity<Cadastro>().HasOne(t => t.Pedido);
+
+        }
+    }
+}
---
+++ Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CasaDoCodigo.Aula1;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace CasaDoCodigo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            //pega a conexão default
+            string connectionString = Configuration.GetConnectionString("Default");
+
+            //contexto de conexão com a ConnectionString
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(connectionString)
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Pedido}/{action=Carrossel}/{id?}");
+            });
+        }
+    }
+}
